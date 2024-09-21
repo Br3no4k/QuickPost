82868cccf2a8df878117f946288f727455c020c5
br3no4k <brenoakuhn@gmail.com>
2024-09-21 14:22:12 -0300

    Deletando Arquivos Indesejados

--- src/components/postitem/PostItem.js
+++ src/components/postitem/PostItem.js
@@ -6,8 +6,7 @@ export class PostItem extends React.Component {
     return (
       <>
         <div>
-        <img src={imageUrl || 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2ZwNTlyN3ZiOTNpb3p4amRmcHBwd3VibXFkb3Q5NTluOWp5NW5qZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/25LbA5gcDNM5N7sHHy/giphy.gif'} alt="Post"/>
-        </div>
+        <img src={imageUrl || ''
         <div>
           <h3>{title}</h3>
           <p>{content}</p>
